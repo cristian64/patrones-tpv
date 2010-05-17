2f028fe7e5cc89de9e33a532a1a05ee6b7bd4fbe
cristian64 <cristian64@3be5725b-6561-fea3-0f3f-8ea076da5dd2>
2010-05-17 14:05:10 +0000

    Venta y LineaVenta "listo".

--- Netbeans TPV/src/LineaVenta.java
+++ Netbeans TPV/src/LineaVenta.java
@@ -1,12 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jorge
- */
-public class LineaVenta {
-
+public class LineaVenta
+{
+	private Producto producto;
+	private int cantidad;
+	
+	public LineaVenta()
+	{
+		producto = null;
+		cantidad = 0;
+	}
+	
+	public Producto getProducto()
+	{
+		return producto;
+	}
+	
+	public int getCantidad()
+	{
+		return cantidad;
+	}
+	
+	public void setProducto(Producto producto)
+	{
+		this.producto = producto;
+	}
+	
+	public void setCantidad(int cantidad)
+	{
+		this.cantidad = cantidad;
+	}
 }
+
--- Netbeans TPV/src/Venta.java
+++ Netbeans TPV/src/Venta.java
@@ -1,12 +1,58 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.ArrayList;
+import java.util.Iterator;
 
-/**
- *
- * @author jorge
- */
-public class Venta {
-
+public class Venta
+{
+	private ArrayList<LineaVenta> lineasVenta;
+	private TipoCliente tipoCliente;
+	
+	public Venta()
+	{
+		lineasVenta = new ArrayList<LineaVenta>();
+		tipoCliente = null;		
+	}
+	
+	public ArrayList<LineaVenta> getLineasVenta()
+	{
+		return lineasVenta;
+	}
+	
+	public void anadirProducto(Producto producto, int cantidad)
+	{
+		LineaVenta nuevaLineaVenta = new LineaVenta();
+		nuevaLineaVenta.setProducto(producto);
+		nuevaLineaVenta.setCantidad(cantidad);
+		lineasVenta.add(nuevaLineaVenta);
+	}
+	
+	public void quitarProducto(Producto producto, int cantidad)
+	{
+		Iterator<LineaVenta> i = lineasVenta.iterator();
+		while (i.hasNext() && cantidad > 0)
+		{
+			LineaVenta lineaVentaActual = i.next();
+			
+			if (lineaVentaActual.getProducto().getId() == producto.getId())
+			{
+				if (lineaVentaActual.getCantidad() > cantidad)
+				{
+					// Si en la línea actual hay más cantidad que la que se quiere sustraer, se decrementa.
+					lineaVentaActual.setCantidad(lineaVentaActual.getCantidad() - cantidad);
+					break;
+				}
+				else
+				{
+					// Si hay menos cantidad, se debe eliminar la línea y continuar la búsqueda del producto en otra línea.
+					cantidad -= lineaVentaActual.getCantidad();
+					i.remove(); //TODO falta comprobar que se elimina bien con "remove", pero como no tengo netbeans ni siquiera sé si existe el método "remove" en el Iterator.				
+				}
+			}
+		}
+	}
+	
+	public void setTipoCliente(TipoCliente tipoCliente)
+	{
+		this.tipoCliente = tipoCliente;
+	}
 }
+
