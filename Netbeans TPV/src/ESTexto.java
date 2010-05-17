99cd9a0d437c46b07900d9d89706740465da73ca
jcz1710 <jcz1710@3be5725b-6561-fea3-0f3f-8ea076da5dd2>
2010-05-17 17:39:16 +0000

    Creada la jerarquia de clases de Entrada-Salida.

---
+++ Netbeans TPV/src/ESTexto.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jorge
+ */
+public class ESTexto implements EntradaSalida {
+
+	public void LeerCatalogo(CatalogoProducto catalogo) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void LeerCatalogoIVA(CatalogoProducto catalogo) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void LeerReglasPrecios(TiposCliente tiposCliente) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void LeerDescuentoVolumen(CatalogoProducto catalogo) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void LeerVenta(Venta venta) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void EscribirVenta(Venta venta) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+}
---
+++ Netbeans TPV/src/EntradaSalida.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jorge
+ */
+public interface EntradaSalida {
+	public void LeerCatalogo(CatalogoProducto catalogo);
+	public void LeerCatalogoIVA(CatalogoProducto catalogo);
+	public void LeerReglasPrecios(TiposCliente tiposCliente);
+	public void LeerDescuentoVolumen(CatalogoProducto catalogo);
+	public void LeerVenta(Venta venta);
+	public void EscribirVenta(Venta venta);
+}
+
