04cd4eb3877b987fee51cd6dfb36f73989eedc17
cristian64 <cristian64@3be5725b-6561-fea3-0f3f-8ea076da5dd2>
2010-05-17 16:53:23 +0000

    Faltaba public en la interfaz de AlgoritmoImpuestos.

--- Netbeans TPV/src/AlgoritmoImpuestos.java
+++ Netbeans TPV/src/AlgoritmoImpuestos.java
@@ -1,12 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jorge
- */
-interface AlgoritmoImpuestos {
-    public double calcularImpuestos(Venta venta);
+public interface AlgoritmoImpuestos
+{
+	public double calcularImpuestos(Venta venta);
 }
