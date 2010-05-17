db39031e22d5d8fd01182415ceb754dd1d64930d
cristian64 <cristian64@3be5725b-6561-fea3-0f3f-8ea076da5dd2>
2010-05-17 16:16:40 +0000

    ImpuestoFijo hecho.

--- Netbeans TPV/src/ImpuestoFijo.java
+++ Netbeans TPV/src/ImpuestoFijo.java
@@ -1,9 +1,9 @@
 public class ImpuestoFijo implements AlgoritmoImpuestos
 {
-    private static final double porcentaje = 16;
-    
-    public double calcularImpuestos(Venta venta) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	private static final double porcentaje = 16;
 
+	public double calcularImpuestos(Venta venta)
+	{
+		return (venta.getPrecioFinal() / (1 + porcentaje/100)) * (porcentaje/100);
+	}
 }
