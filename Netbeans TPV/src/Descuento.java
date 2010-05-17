eb756f9fc6f7db5dca9476a7da4bdbd5f75aafbf
cristian64 <cristian64@3be5725b-6561-fea3-0f3f-8ea076da5dd2>
2010-05-17 16:24:39 +0000

    Faltaba "public" en la interfaz de Descuento; daba un aviso en otro lado.

--- Netbeans TPV/src/Descuento.java
+++ Netbeans TPV/src/Descuento.java
@@ -1,12 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jorge
- */
-interface Descuento {
-    public double calcularDescuento(Venta venta);
+public interface Descuento
+{
+	public double calcularDescuento(Venta venta);
 }
